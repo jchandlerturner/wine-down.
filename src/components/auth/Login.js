07c8e78638d19fc75791c975ea4f2808828f31ec
Chandler Turner <chandlerturner@Chandlers-MacBook-Air.local>
2019-10-21 09:46:09 -0500

    login screen populates

---
+++ src/components/auth/Login.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react"
+import LoginManager from "../../modules/LoginManager";
+
+
+class Login extends Component {
+
+    // Set initial state
+
+    state = {
+        email: "",
+        password: "",
+        id: "",
+    }
+
+    handleFieldChange = (evt) => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    handleLogin = (e) => {
+        e.preventDefault()
+        LoginManager.getUserData("users").then((users) => {
+            let singleUser = users.find(
+                user =>
+                    user.password.toLowerCase() === this.state.password.toLowerCase() &&
+                    user.email.toLowerCase() === this.state.email.toLowerCase()
+            );
+            if (this.state.email === "") {
+                window.alert("Please enter email")
+            } else if (this.state.password === "") {
+                window.alert("Please enter password")
+            } else if (singleUser) {
+                this.props.setUser(singleUser);
+            } else {
+                window.alert("User email and password do not match")
+            }
+        })
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleLogin}>
+                <fieldset>
+                    <h3>Please log in</h3>
+                    <div className="formgrid">
+                        <input onChange={this.handleFieldChange} type="email"
+                            id="email"
+                            placeholder="Email address"
+                            required="" autoFocus="" />
+                        <label htmlFor="inputEmail">Email address</label>
+
+                        <input onChange={this.handleFieldChange} type="password"
+                            id="password"
+                            placeholder="Password"
+                            required="" />
+                        <label htmlFor="inputPassword">Password</label>
+                    </div>
+                    <button type="submit" className="submit">
+                        Log in
+                </button>
+                </fieldset>
+            </form>
+        )
+    }
+}
+
+export default Login 
